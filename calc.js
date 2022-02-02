1eaddc10a8972416bd10c03fe7a5e279a13efb71
hex0b1t <git.cybergod.repo@gmail.com>
2022-02-02 16:04:44 +0100

    Added logic to the javascript ... im tired lmaoo

--- calc.js
+++ calc.js
@@ -1,38 +1,27 @@
 window.addEventListener('DOMContentLoaded', function() {
-    const numberBtns = document.querySelectorAll('.numbers')
-    const operatorBtns = document.querySelector('.operator')
-    let operationDisplay = document.querySelector('#calc-operations')
-    let resultDisplay = document.querySelector('#calc-result')
+    const number_btns = document.querySelectorAll('.numbers')
+    const operator_btns = document.querySelectorAll('.operator')
+    let operations_display = document.querySelector('#calc-operations')
+    let output_display = document.querySelector('#calc-result')
     
-    let first, second
-
-    const calculator = {
-        operators: [
-            'x', '+', '-', '/'
-        ],
-
-        add(f, s) {
-            return f + s
-        },
-
-        sub(f, s) {
-            return f - s
-        },
-
-        mul(f, s) {
-            return f * s
-        },
-
-        div(f, s) {
-            return f / s
-        }
-    }
-
-    numberBtns.forEach(btn => {
-        btn.addEventListener('click', function(e) {
-            operationDisplay.innerText += btn.innerText
-            first = Number(btn.innerText)
-            
+    number_btns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            operations_display.innerText += btn.innerText
         })
     })
+
+    operator_btns.forEach(op_btn => {
+        op_btn.addEventListener('click', function() {
+            if(this.id != 'equal') {
+                if(this.innerText == 'x') operations_display.innerText += '*'
+                else operations_display.innerText += this.innerText
+            }
+            output_display.innerText = formatted_output(eval(operations_display.innerText))
+        })
+    })
+
+    function formatted_output(num) {
+        return num.toLocaleString("en")
+    }
+
 })
